29ea4ff3112d481d8af440323790b76425d29a17
Abhirag <abhiragsv2005@gmail.com>
2025-08-30 20:25:26 +0530

    added about us and contact us icon on menu items

--- Ganesh_Cart/components/Navbar.jsx
+++ Ganesh_Cart/components/Navbar.jsx
@@ -6,6 +6,19 @@ import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
 import { useClerk, UserButton } from "@clerk/nextjs";
 
+// Icon components for mobile menu
+const AboutIcon = () => (
+  <svg className="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M12 21a9 9 0 1 1 0-18 9 9 0 0 1 0 18Zm0-13v4m0 4h.01" />
+  </svg>
+);
+
+const ContactIcon = () => (
+  <svg className="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M7.978 4a2.553 2.553 0 0 0-1.926.877C4.233 6.7 3.699 8.751 4.153 10.814c.44 1.995 1.778 3.893 3.456 5.572 1.68 1.679 3.577 3.018 5.57 3.459 2.062.456 4.115-.073 5.94-1.885a2.556 2.556 0 0 0 .001-3.861l-1.21-1.21a2.689 2.689 0 0 0-3.802 0l-.617.618a.806.806 0 0 1-1.14 0l-1.854-1.855a.807.807 0 0 1 0-1.14l.618-.62a2.692 2.692 0 0 0 0-3.803l-1.21-1.211A2.555 2.555 0 0 0 7.978 4Z" />
+  </svg>
+);
+
 /**
  * Navbar Component
  * Main navigation bar with responsive design, search functionality, cart, and wishlist
@@ -162,6 +175,12 @@ const Navbar = () => {
               </UserButton.MenuItems>
               <UserButton.MenuItems>
                 <UserButton.Action label="Products" labelIcon={<BoxIcon/>} onClick={()=>router.push('/all-products')}/>
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action label="About Us" labelIcon={<AboutIcon/>} onClick={()=>router.push('/aboutus')}/>
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action label="Contact" labelIcon={<ContactIcon/>} onClick={()=>router.push('/Contact')}/>
               </UserButton.MenuItems>
               {/* Wishlist navigation - Mobile user menu */}
               <UserButton.MenuItems>
